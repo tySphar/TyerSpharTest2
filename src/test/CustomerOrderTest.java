a55b68e6e67f6cd6f8cbde3b00e948c82fa109fd
tyler <tyler@Tys>
2025-03-30 15:06:29 -0400

    Test for low stock alert

--- src/test/CustomerOrderTest.java
+++ src/test/CustomerOrderTest.java
@@ -51,6 +51,11 @@ public class CustomerOrderTest {
 
 	// ToDo: Test low stock alert
 	// Reduce the stock of an item and verify that the system triggers a low stock alert when the quantity falls below the threshold.
+    @Test
+	public void testLowStockAlert() {
+	    inventory.updateStock("Garlic Bread", 13);  // Only 2 left after this
+	    assertTrue(inventory.isLowStock("Garlic Bread"));
+	}
 
 
 	// ToDo: Test order status updates
